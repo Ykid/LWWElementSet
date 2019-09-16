11edf7ed4bee951239e2646a2a71ed0c81085232
test <test@test.com>
2019-09-16 12:14:25 +0800

    add more test cases

--- src/test/scala/com/challenge/LWWElementSetSpec.scala
+++ src/test/scala/com/challenge/LWWElementSetSpec.scala
@@ -9,30 +9,60 @@ class LWWElementSetSpec extends FunSpec with Matchers {
    */
   describe("A LWWElementSet") {
     describe("basic add functionality") {
-      it ("should be able to add one element when empty") {
+      it("should be able to add one element when empty") {
         val set = new LWWElementSet()
-        set.add(1).result() should === (Set(1))
+        set.add(1).result() should ===(Set(1))
       }
 
-      it ("should add duplicate element only once") {
+      it("should add duplicate element only once") {
         val set = new LWWElementSet()
-        set.add(1).add(1).result() should === (Set(1))
+        set.add(1).add(1).result() should ===(Set(1))
+      }
+
+      it("should be able to add different elements") {
+        val set = new LWWElementSet()
+        set.add(1).add(2).add(3).result() should ===(Set(1,2,3))
       }
     }
 
     describe("basic remove functionality") {
       it("should be able remove element if the set contains it") {
         val set = new LWWElementSet()
-        set.add(1).remove(1).result() should === (Set())
+        set.add(1).remove(1).result() should ===(Set())
       }
 
       it("should do nothing it the element to be removed is not in the set") {
         val set = new LWWElementSet()
-        set.add(1).remove(2).result() should === (Set(1))
+        set.add(1).remove(2).result() should ===(Set(1))
       }
     }
 
     describe("merge functionality") {
+      it("case one: different elements will appear in the merge set") {
+        val set1 = new LWWElementSet().add(1)
+        val set2 = new LWWElementSet().add(2)
+        set1.merge(set2).result() should ===(Set(1,2))
+      }
+
+      it("case two: same elements will not be duplicated") {
+        val set1 = new LWWElementSet().add(1)
+        val set2 = new LWWElementSet().add(1)
+        set1.merge(set2).result() should ===(Set(1))
+      }
+
+      it("case three: if element in set1 is deleted in the other set, the element will not shown") {
+        val set1 = new LWWElementSet().add(1)
+        Thread.sleep(5)
+        val set2 = new LWWElementSet().add(1).remove(1)
+        set1.merge(set2).result() should ===(Set())
+      }
+
+      it("case four: if deleted element in set1 is added back in the other set, the element will be shown") {
+        val set1 = new LWWElementSet().add(1).remove(1)
+        Thread.sleep(5)
+        val set2 = new LWWElementSet().add(1)
+        set1.merge(set2).result() should ===(Set(1))
+      }
     }
   }
 }
